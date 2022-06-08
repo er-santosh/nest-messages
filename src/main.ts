ee8740cd0d6c1fa5529b10f890e9334a951db399
Er Santosh <er.santoshtharu@gmail.com>
2022-06-08 20:49:34 +0545

    Validating request data with pipes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe()); //only validates if validation rules are added
   await app.listen(3000);
 }
 bootstrap();
